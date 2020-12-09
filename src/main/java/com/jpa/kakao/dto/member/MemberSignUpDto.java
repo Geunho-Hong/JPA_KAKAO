fd82f0d8fa12120f423370f07286c7776e5b6c5c
GuenhoHong <hogu8159@naver.com>
2020-12-09 23:39:29 +0900

    Spring @Valid message 추가

--- src/main/java/com/jpa/kakao/dto/member/MemberSignUpDto.java
+++ src/main/java/com/jpa/kakao/dto/member/MemberSignUpDto.java
@@ -19,23 +19,23 @@ public class MemberSignUpDto {
     // @NotEmpty : 반드시 값이 존재하고 길이 혹은 크기가 0보다 커야한다
     // @NotBrank : 반드시 값이 존재하고 공백 문자를 제외한 길이가 0보다 커야 한다
 
-    @NotEmpty
+    @NotEmpty(message =  "카카오 Id를 입력해주세요")
     private String kakaoId;
 
-    @NotEmpty
+    @NotEmpty(message = "패스워드를 입력해주세요")
     private String password;
 
-    @NotEmpty
+    @NotEmpty(message = "이메일을 입력해주세요")
     @Email
     private String email;
 
-    @NotEmpty
+    @NotEmpty(message = "이름을 입력해주세요")
     private String name;
 
-    @NotEmpty
+    @NotEmpty(message = "전화번호를 입력해주세요")
     private String phoneNumber;
 
-    @NotNull
+    @NotNull(message = "생년월일을 입력해주세요")
     private LocalDateTime birthDate;
 
     public Member toMemberEntity() {
